38d87ea1bf29e5c64f3263774e7ab24fc25607ab
Luke Jang <lukejang@LukeMBP.local>
2016-08-26 23:15:07 +0800

    Create a video chat example project

---
+++ Examples/VideoChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoChat
+//
+//  Created by Luke Jang on 8/26/16.
+//  Copyright © 2016 StraaS.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/VideoChat/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  VideoChat
+//
+//  Created by Luke Jang on 8/26/16.
+//  Copyright © 2016 StraaS.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
