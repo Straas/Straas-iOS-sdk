e88dc58df8be3d5e77ba4eac1240096e9036a918
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-09 17:56:25 +0800

    feat: make ChatStickerViewController can easily be customized by subclassing.

--- Examples/VideoChat/ChatRoom/ChatStickerViewController.h
+++ Examples/VideoChat/ChatRoom/ChatStickerViewController.h
@@ -7,23 +7,161 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ChatViewController.h"
 #import <StraaSMessagingSDK/STSChatroomConnectionOptions.h>
 
-@class ChatViewController;
+/**
+ ChatStickerViewController is a StraaS.io chatroom UI with sticker input. It is meant to be subclassed.
+ If you don't want to use our sticker input feature, use ChatViewController instead.
+ */
+@interface ChatStickerViewController : UIViewController <STSChatEventDelegate, ChatStickerDelegate>
 
-@interface ChatStickerViewController : UIViewController
+/**
+ ChatVC is kind of ChatViewController object.
+ You may customize its UI by setting its property.
+ */
+@property (nonatomic, readonly) id chatVC;
 
-@property (nonatomic, readonly) ChatViewController * chatVC;
+/**
+ Current member token.
+ */
+@property (nonatomic, readonly) NSString * JWT;
 
+/**
+ Current chatroom name.
+ */
+@property (nonatomic, readonly) NSString * chatroomName;
+
+/**
+ Current connection options.
+ */
+@property (nonatomic, readonly) STSChatroomConnectionOptions connectionOptions;
+
+/**
+ StraaS Chat manager.
+ */
+@property (nonatomic, readonly) STSChatManager * manager;
+
+/**
+ StickerViewShowingHeight is the height of stickerView as stickerView is showing.
+ */
+@property (nonatomic) NSUInteger stickerViewShowingHeight;
+
+/**
+ Return a ChatStickerViewController object with default ChatViewController object as property.
+ If you wanna override ChatViewController's method, use chatStickerViewControllerWithJWT:chatroomName:connectionOptions:chatViewController: instead.
+
+ @param JWT The StraaS member token.
+ @param chatroomName The chatroom name you want to connect to.
+ @param connectionOptions The connection options to connect to the chat you want.
+ @return An instance of ChatStickerViewController.
+ */
 + (instancetype)chatStickerViewControllerWithJWT:(NSString *)JWT
                                     chatroomName:(NSString *)chatroomName
                                connectionOptions:(STSChatroomConnectionOptions)connectionOptions;
 
+/**
+ Return a ChatStickerViewController object with subClass of ChatViewController as property.
+ If you do not have the needs to subclass ChatViewController, use chatStickerViewControllerWithJWT:chatroomName:connectionOptions: instead.
+ 
+ @param JWT The StraaS member token.
+ @param chatroomName The chatroom name you want to connect to.
+ @param connectionOptions The connection options to connect to the chat you want.
+ @param chatViewController A subclass of ChatViewController object.
+ @return An instance of ChatStickerViewController.
+ */
++ (instancetype)chatStickerViewControllerWithJWT:(NSString *)JWT
+                                    chatroomName:(NSString *)chatroomName
+                               connectionOptions:(STSChatroomConnectionOptions)connectionOptions
+                              chatViewController:(id)chatViewController;
+
+/**
+ Return a ChatStickerViewController object initialized from JWT, chatroomName and connectionOptionsjo.
+ If you wanna override ChatViewController's method, use chatStickerViewControllerWithJWT:chatroomName:connectionOptions:chatViewController: instead.
+ 
+ @param JWT The StraaS member token.
+ @param chatroomName The chatroom name you want to connect to.
+ @param connectionOptions The connection options to connect to the chat you want.
+ @return An instance of ChatStickerViewController.
+ */
 - (instancetype)initWithJWT:(NSString *)JWT
                chatroomName:(NSString *)chatroomName
           connectionOptions:(STSChatroomConnectionOptions)connectionOptions;
 
+/**
+ Return a ChatStickerViewController object initialized from JWT, chatroomName and connectionOptionsjo.
+ If you do not have the needs to subclass ChatViewController, use chatStickerViewControllerWithJWT:chatroomName:connectionOptions: instead.
+ 
+ @param JWT The StraaS member token.
+ @param chatroomName The chatroom name you want to connect to.
+ @param connectionOptions The connection options to connect to the chat you want.
+ @param chatViewController A subClass of ChatViewController object.
+ @return An instance of ChatStickerViewController.
+ */
+- (instancetype)initWithJWT:(NSString *)JWT
+               chatroomName:(NSString *)chatroomName
+          connectionOptions:(STSChatroomConnectionOptions)connectionOptions
+         chatViewController:(id)chatViewController;
+
+/**
+ Configure application to use StraaS.io serveice.
+
+ @param success A block object to be executed when the configuration finishes successfully.
+ @param failure A block object to be executed when the task finishes unsuccessfully.
+ */
+- (void)configureApplication:(void(^)())success
+                     failure:(void(^)(NSError * error))failure NS_REQUIRES_SUPER;
+
+/**
+ Connect to the chatroom with current JWT, chatroom name, and connection options.
+ */
+- (void)connectToChat NS_REQUIRES_SUPER;
+
+/**
+ Connect to the chatroom with specific JWT, chatroom name, and connection options.
+ Note: ChatStickerViewController is designed to connect one chatroom only.
+ This method would disconnect current connection and make a new connection to the chatroom.
+
+ @param JWT The StraaS member JWT.
+ @param chatroomName The chatroom name you want to connect to.
+ @param connectionOptions The connection options to connect to the chat you want.
+ */
+- (void)connectToChatWithJWT:(NSString *)JWT
+                chatroomName:(NSString *)chatroomName
+           connectionOptions:(STSChatroomConnectionOptions)connectionOptions NS_REQUIRES_SUPER;
+
+
 + (instancetype)new __attribute__((unavailable("Use chatStickerViewControllerWithJWT:chatroomName:connectionOptions: instead.")));
 - (instancetype)init  __attribute__((unavailable("Use initWithJWT:chatroomName:connectionOptions: instead.")));
+
+#pragma mark - STSChatEventDelegate
+/**
+ The following methods are as same as STSChatEventDelegate.
+ You may override any of this to do some customizations when the method is called.
+*/
+- (void)chatroomConnected:(STSChat *)chatroom NS_REQUIRES_SUPER;
+- (void)chatroomDisconnected:(STSChat *)chatroom NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom failToConnect:(NSError *)error NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom error:(NSError *)error NS_REQUIRES_SUPER;
+- (void)chatroomInputModeChanged:(STSChat *)chatroom NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom usersJoined:(NSArray<STSChatUser *> *)users NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom usersUpdated:(NSArray<STSChatUser *> *)users NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom usersLeft:(NSArray<NSNumber *> *)userLabels NS_REQUIRES_SUPER;
+- (void)chatroomUserCount:(STSChat *)chatroom NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom messageAdded:(STSChatMessage *)message NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom messageRemoved:(NSString *)messageId NS_REQUIRES_SUPER;
+- (void)chatroomMessageFlushed:(STSChat *)chatroom NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom aggregatedDataAdded:(NSDictionary *)aggregatedData NS_REQUIRES_SUPER;
+- (void)chatroom:(STSChat *)chatroom rawDataAdded:(id)rawData NS_REQUIRES_SUPER;
+
+#pragma mark - ChatStickerDelegate
+/**
+ The following methods are as same as ChatStickerDelegate.
+ You may override any of this to do some customizations when the method is called.
+ */
+- (void)chatStickerDidLoad:(NSArray *)stickers NS_REQUIRES_SUPER;
+- (void)showStickerView:(BOOL)animated NS_REQUIRES_SUPER;
+- (void)dismissStickerView:(BOOL)animated NS_REQUIRES_SUPER;
+- (BOOL)isStickerViewShowing NS_REQUIRES_SUPER;
 
 @end
--- Examples/VideoChat/ChatRoom/ChatStickerViewController.m
+++ Examples/VideoChat/ChatRoom/ChatStickerViewController.m
@@ -10,10 +10,13 @@
 #import "ChatViewController.h"
 #import "StickersInputView.h"
 
-@interface ChatStickerViewController ()<ChatStickerDelegate>
+NSUInteger const defaultStickerViewHeight = 215;
+
+@interface ChatStickerViewController ()
 @property (nonatomic) StickersInputView * stickerView;
 @property (nonatomic, getter=isStickerViewShowing) BOOL stickerViewShowing;
 @property (nonatomic) NSLayoutConstraint * stickerHeightConstraint;
+@property (nonatomic) NSUInteger stickerViewHeight;
 @property (nonatomic) ChatViewController * chatVC;
 @end
 
@@ -26,6 +29,18 @@
     [[ChatStickerViewController alloc] initWithJWT:JWT
                                       chatroomName:chatroomName
                                  connectionOptions:connectionOptions];
+    return viewController;
+}
+
++ (instancetype)chatStickerViewControllerWithJWT:(NSString *)JWT
+                                    chatroomName:(NSString *)chatroomName
+                               connectionOptions:(STSChatroomConnectionOptions)connectionOptions
+                              chatViewController:(id)chatViewController {
+    ChatStickerViewController * viewController =
+    [[ChatStickerViewController alloc] initWithJWT:JWT
+                                      chatroomName:chatroomName
+                                 connectionOptions:connectionOptions
+                                chatViewController:chatViewController];
     return viewController;
 }
 
@@ -43,6 +58,19 @@
     return self;
 }
 
+- (instancetype)initWithJWT:(NSString *)JWT
+               chatroomName:(NSString *)chatroomName
+          connectionOptions:(STSChatroomConnectionOptions)connectionOptions
+         chatViewController:(id)chatViewController {
+    NSAssert([chatViewController isKindOfClass:[chatViewController class]],
+             @"chatViewController should be kind of ChatViewController object.");
+    if (self = [super init]) {
+        self.chatVC = chatViewController;
+        [self commonInit];
+    }
+    return self;
+}
+
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
@@ -53,11 +81,12 @@
 }
 
 - (void)commonInit {
+    self.chatVC.eventDelegate = self;
+    self.chatVC.delegate = self;
     self.chatVC.view.translatesAutoresizingMaskIntoConstraints = NO;
     [self addChildViewController:self.chatVC];
     [self.view addSubview:self.chatVC.view];
     [self.view addSubview:self.stickerView];
-    self.chatVC.delegate = self;
     self.stickerView.delegate = self.chatVC;
     NSDictionary * views = @{@"chatVC": self.chatVC.view,
     @"stickerView": self.stickerView};
@@ -68,12 +97,59 @@
     [NSLayoutConstraint activateConstraints:constraints];
     constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[chatVC]-0-[stickerView]-0-|" options:0 metrics:nil views:views];
     [NSLayoutConstraint activateConstraints:constraints];
-    [self setStickerViewHeight:0.0];
-    
+    [self setStickerViewHeight:0];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+#pragma mark - accessor.
+
+- (StickersInputView *)stickerView {
+    if (!_stickerView) {
+        _stickerView = [[[NSBundle mainBundle] loadNibNamed:kStickersInputView owner:self options:nil] objectAtIndex:0];
+        _stickerView.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+    return _stickerView;
+}
+
+- (NSString *)JWT {
+    return self.chatVC.JWT;
+}
+
+- (NSString *)chatroomName {
+    return self.chatVC.chatroomName;
+}
+
+- (STSChatroomConnectionOptions)connectionOptions {
+    return self.chatVC.connectionOptions;
+}
+
+- (STSChatManager *)manager {
+    return self.chatVC.manager;
+}
+
+- (NSUInteger)stickerViewShowingHeight {
+    if (!_stickerViewShowingHeight) {
+        _stickerViewShowingHeight = defaultStickerViewHeight;
+    }
+    return _stickerViewShowingHeight;
+}
+
+#pragma mark - Public methods
+- (void)configureApplication:(void (^)())success failure:(void (^)(NSError *))failure {
+    [self.chatVC configureApplication:^(BOOL succeedConfiguration, NSError *error) {
+        succeedConfiguration? success():failure(error);
+    }];
+}
+
+- (void)connectToChat {
+    [self.chatVC connectToChat];
+}
+
+- (void)connectToChatWithJWT:(NSString *)JWT chatroomName:(NSString *)chatroomName connectionOptions:(STSChatroomConnectionOptions)connectionOptions {
+    [self.chatVC connectToChatWithJWT:JWT chatroomName:chatroomName connectionOptions:connectionOptions];
 }
 
 - (void)chatStickerDidLoad:(NSArray *)stickers {
@@ -82,7 +158,7 @@
 
 - (void)showStickerView:(BOOL)animated {
     self.stickerViewShowing = YES;
-    [self setStickerViewHeight:215];
+    [self setStickerViewHeight:self.stickerViewShowingHeight];
     if (animated) {
         [UIView animateWithDuration:0.3 animations:^{
             [self.view layoutIfNeeded];
@@ -104,7 +180,12 @@
     }
 }
 
-- (void)setStickerViewHeight:(CGFloat)height {
+- (void)setStickerViewHeight:(NSUInteger)stickerViewHeight {
+    _stickerViewHeight = stickerViewHeight;
+    [self updateStickerViewHeight:stickerViewHeight];
+}
+
+- (void)updateStickerViewHeight:(CGFloat)height {
     if (self.stickerHeightConstraint) {
         [NSLayoutConstraint deactivateConstraints:@[self.stickerHeightConstraint]];
     }
@@ -113,14 +194,62 @@
     [NSLayoutConstraint activateConstraints:@[self.stickerHeightConstraint]];
 }
 
-#pragma mark - accessor.
+#pragma mark - STSChatEvnetDelegate
 
-- (StickersInputView *)stickerView {
-    if (!_stickerView) {
-        _stickerView = [[[NSBundle mainBundle] loadNibNamed:kStickersInputView owner:self options:nil] objectAtIndex:0];
-        _stickerView.translatesAutoresizingMaskIntoConstraints = NO;
-    }
-    return _stickerView;
+- (void)chatroomConnected:(STSChat *)chatroom {
+    [self.chatVC chatroomConnected:chatroom];
+}
+
+- (void)chatroomDisconnected:(STSChat *)chatroom {
+    [self.chatVC chatroomDisconnected:chatroom];
+}
+
+- (void)chatroom:(STSChat *)chatroom failToConnect:(NSError *)error {
+    [self.chatVC chatroom:chatroom failToConnect:error];
+}
+
+- (void)chatroom:(STSChat *)chatroom error:(NSError *)error {
+    [self.chatVC chatroom:chatroom error:error];
+}
+
+- (void)chatroomInputModeChanged:(STSChat *)chatroom {
+    [self.chatVC chatroomInputModeChanged:chatroom];
+}
+
+- (void)chatroom:(STSChat *)chatroom usersJoined:(NSArray<STSChatUser *> *)users {
+    [self.chatVC chatroom:chatroom usersJoined:users];
+}
+
+- (void)chatroom:(STSChat *)chatroom usersUpdated:(NSArray<STSChatUser *> *)users {
+    [self.chatVC chatroom:chatroom usersUpdated:users];
+}
+
+- (void)chatroom:(STSChat *)chatroom usersLeft:(NSArray<NSNumber *> *)userLabels {
+    [self.chatVC chatroom:chatroom usersLeft:userLabels];
+}
+
+- (void)chatroomUserCount:(STSChat *)chatroom {
+    [self.chatVC chatroomUserCount:chatroom];
+}
+
+- (void)chatroom:(STSChat *)chatroom messageAdded:(STSChatMessage *)message {
+    [self.chatVC chatroom:chatroom messageAdded:message];
+}
+
+- (void)chatroom:(STSChat *)chatroom messageRemoved:(NSString *)messageId {
+    [self.chatVC chatroom:chatroom messageRemoved:messageId];
+}
+
+- (void)chatroomMessageFlushed:(STSChat *)chatroom {
+    [self.chatVC chatroomMessageFlushed:chatroom];
+}
+
+- (void)chatroom:(STSChat *)chatroom aggregatedDataAdded:(NSDictionary *)aggregatedData {
+    [self.chatVC chatroom:chatroom aggregatedDataAdded:aggregatedData];
+}
+
+- (void)chatroom:(STSChat *)chatroom rawDataAdded:(id)rawData {
+    [self.chatVC chatroom:chatroom rawDataAdded:rawData];
 }
 
 @end
