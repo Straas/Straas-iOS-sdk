c016a2f9d8eb6970d2f8f2c88b41ae4287d781f6
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-26 13:36:52 +0800

    docs: add docs for StickerInputViewDelegate

--- Examples/VideoChat/ChatRoom/StickerInputViewDelegate.h
+++ Examples/VideoChat/ChatRoom/StickerInputViewDelegate.h
@@ -8,7 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ The delegate of a NSObject must adopt the StickerInputViewDelegate protocol.
+ */
 @protocol StickerInputViewDelegate <NSObject>
+
+/**
+ Tells the delegate that a sticker key did be selected.
+
+ @param key The sticker key name.
+ @param imageURL The sticker image URL.
+ */
 - (void)didSelectStickerKey:(NSString *)key
                    imageURL:(NSString *)imageURL;
+
 @end
