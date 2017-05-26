c959f0af2b0db3ba77646069e71c359e97768e0b
Jerome1210 <b99705008@ntu.edu.tw>
2017-05-26 17:40:27 +0800

    feat: add DataChannelEventDelegate dataChannelDelegate in TransparentChatViewController
    also add more log to debug.

--- Examples/VideoChat/ChatRoom/TransparentChatViewController.h
+++ Examples/VideoChat/ChatRoom/TransparentChatViewController.h
@@ -9,8 +9,17 @@
 #import <UIKit/UIKit.h>
 #import "ChatViewController.h"
 
+@protocol DataChannelEventDelegate <NSObject>
+
+- (void)aggregatedItemsAdded:(NSArray<STSAggregatedItem *> *)aggregatedItems;
+- (void)rawDataAdded:(STSChatMessage *)rawData;
+
+@end
+
 static NSString *TransparentMessengerCellIdentifier = @"TransparentMessengerCell";
 
 @interface TransparentChatViewController : ChatViewController
+
+@property (weak, nonatomic) id<DataChannelEventDelegate> dataChannelDelegate;
 
 @end
