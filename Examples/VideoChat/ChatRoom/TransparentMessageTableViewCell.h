82a70b43cc01a14d4d2e8fda6c79f028aa4978fe
Harry Hsu <harryxu36@gmail.com>
2017-05-26 11:55:34 +0800

    feat: Add TransparentMessageTableViewCell class

---
+++ Examples/VideoChat/ChatRoom/TransparentMessageTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TransparentMessageTableViewCell.h
+//  VideoChat
+//
+//  Created by Harry on 25/05/2017.
+//  Copyright © 2017 StraaS.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransparentMessageTableViewCell : UITableViewCell
+
+- (void)setBodyAttributedText:(NSAttributedString*)text;
+
+@end
