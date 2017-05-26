1cb7a4d84d7e78dc62c8ed789c0f7b86acecc237
Harry Hsu <harryxu36@gmail.com>
2017-05-26 12:04:20 +0800

    feat: Add AdvancedChatViewController class

---
+++ Examples/VideoChat/ChatRoom/AdvancedChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  AdvancedChatViewController.h
+//  VideoChat
+//
+//  Created by Harry on 25/05/2017.
+//  Copyright © 2017 StraaS.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <StraaSMessagingSDK/StraaSMessagingSDK.h>
+
+@interface AdvancedChatViewController : UIViewController
+
+- (void)connectToChatWithJWT:(NSString *)JWT chatroomName:(NSString *)chatroomName connectionOptions:(STSChatroomConnectionOptions)connectionOptions;
+
+@end
