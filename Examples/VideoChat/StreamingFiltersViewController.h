1d48d31af2d0a53f537bca1514f13cad7f633ac2
Allen Lin <allenlinli@gmail.com>
2019-01-28 09:01:07 +0800

    feat: add new class StreamingFiltersViewController

---
+++ Examples/VideoChat/StreamingFiltersViewController.h
@@ -0,0 +1,17 @@
+//
+//  StreamingFiltersViewController.h
+//  VideoChat
+//
+//  Created by Allen and Kim on 2019/1/28.
+//  Copyright © 2019 StraaS.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StreamingFiltersViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Examples/VideoChat/StreamingFiltersViewController.m
@@ -0,0 +1,32 @@
+//
+//  StreamingFiltersViewController.m
+//  VideoChat
+//
+//  Created by Allen and Kim on 2019/1/28.
+//  Copyright © 2019 StraaS.io. All rights reserved.
+//
+
+#import "StreamingFiltersViewController.h"
+
+@interface StreamingFiltersViewController ()
+
+@end
+
+@implementation StreamingFiltersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
